1dfbd3d10224fbb09693c51bb0e88b3538639bac
thaiaiai <thaina.fsoares@aluno.edu.es.gov.br>
2025-09-03 11:01:48 -0300

    Create index.php

---
+++ Aula 02/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 02 IPI </title>
+    </head>
+    <body>
+        <?php 
+            $nome="Thaina"; 
+            echo"Meu nome é".$nome;
+        ?>
+    </body>
+</html>
