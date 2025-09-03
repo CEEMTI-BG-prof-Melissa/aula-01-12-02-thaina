8e9483cb855d531cb8efdc104075d6dc3862cda8
thaiaiai <thaina.fsoares@aluno.edu.es.gov.br>
2025-09-03 10:36:51 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, mundo!";
+            echo "Thainá";
+        ?>
+    </body>
+</html>
