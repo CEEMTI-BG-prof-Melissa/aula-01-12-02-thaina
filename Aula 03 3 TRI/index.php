d792e8f9d8464dd82047db57e47385a222f28e97
thaiaiai <thaina.fsoares@aluno.edu.es.gov.br>
2025-09-10 10:32:53 -0300

    Create index.php

---
+++ Aula 03 3 TRI/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+
+            $nome1="Thaina";
+            $nome2="Thaiai";
+
+            $n1=10;
+            $n2=2;
+            $soma= $n1+$n2;
+
+            echo "Meu nome é: " .$nome1  ."e minha dupla é:" .$nome2;
+            echo "<br> Meu nome é: $nome1 e minha dupla é: $nome2.";
+
+            echo "<br> <br> A soma de $n1 com $n2 é igual a $soma."
+
+        ?>
+    </body>
+</html>
