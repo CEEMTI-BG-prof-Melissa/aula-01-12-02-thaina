135420bf83e52d26ecb942ab1783710c1540a772
thaiaiai <thaina.fsoares@aluno.edu.es.gov.br>
2025-09-10 10:56:09 -0300

    Update index.php

--- Aula 03 3 TRI/index.php
+++ Aula 03 3 TRI/index.php
@@ -14,7 +14,15 @@
             echo "Meu nome é: " .$nome1  ."e minha dupla é:" .$nome2;
             echo "<br> Meu nome é: $nome1 e minha dupla é: $nome2.";
 
-            echo "<br> <br> A soma de $n1 com $n2 é igual a $soma."
+            echo "<br><br> A soma de $n1 com $n2 é igual a $soma.";
+
+            echo "<br><br> O Resultado da Subtração de $n1 e $n2 é igual a: " .($n1-$n2) .".";
+
+            echo "<br><br> O Número $n1 Multiplicado por $n2 é igual a: " .($n1*$n2) .".";
+
+            echo "<br><br> O Número $n2 Dividido por $n2 é igual a: " .($n1/$n2) .".";
+
+           echo "<br><br> O Resto do resultado da divisão de $n1 e $n2 é: " .($n1%$n2) .".";
 
         ?>
     </body>
