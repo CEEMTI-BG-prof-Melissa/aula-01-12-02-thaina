241284b4c1d1e19e7ffeb3ded78ec6a4a34e0a77
thaiaiai <thaina.fsoares@aluno.edu.es.gov.br>
2025-09-24 09:08:00 -0300

    Create index.php

---
+++ Aula 05 3 TRI/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> Aula 05 - php </title>
+    </head>
+    <body>
+        <?php
+           
+            $n1= 2;
+            $n2= $_GET["x"];
+           
+            echo "Os valores informados são $n1 e $n2 <br>";
+            echo "O valor absoluto de $n2 é " .abs($n2);
+            echo "<br> o resultado de $n2 <sup> $n1</sup> é igual a " .pow($n2,$n1);
+            echo " <br> A raiz quadrada de $n2 é " .sqrt($n2);
+            echo " <br> O $n2 arredondado fica " .round($n2);
+            echo "<br> O $n2 arredondado para cima fica " .ceil($n2);
+            echo "<br> O $n2 arredondado para baixo fica " .floor($n2);
+        ?>
+    </body>
+</html>
