91a9b507184e1fb18d7d7f974057c7ceec930a92
thaiaiai <thaina.fsoares@aluno.edu.es.gov.br>
2025-10-01 10:36:34 -0300

    Create index.php

---
+++ Aula 06 3 TRI/index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title> Aula 06 </title>
+    </head>
+    <body>
+
+        <h2> Operadores de Atribuição </h2>
+        <hr>
+
+        <?php
+
+            $a=2;
+            $b=$a+5;
+
+            echo"<br> A é igual a $a e B é igual a $b.";
+            echo"<br>";
+            $a=$a+5;
+            echo"Agora A é igual a $a";
+            echo"<br>";
+            $a=$a+3;
+            echo" Agora A é igual a $a.";
+            echo"<br>";
+            $b+=8;
+            echo"Agora B é igual a $b.";
+            echo"<br>";
+            $b-=5;
+            echo"Agora B é igual a $b.";
+        ?>
+    </body>
+</html>
